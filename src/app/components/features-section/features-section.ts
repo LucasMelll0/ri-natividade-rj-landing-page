f2c50a6e4a75e1490965d7fcee1c274f4a16009f
Lucas Mello <77343991+LucasMelll0@users.noreply.github.com>
2025-10-11 11:56:55 -0300

    feat: add new feature items and images to FeaturesSection component

--- src/app/components/features-section/features-section.ts
+++ src/app/components/features-section/features-section.ts
@@ -45,15 +45,41 @@ export class FeaturesSection {
     },
     {
       title: 'Visualização de Satélite de Alta Precisão',
-      description: 'Obtenha uma perspectiva real do terreno com imagens de satélite de alta resolução',
+      description:
+        'Obtenha uma perspectiva real do terreno com imagens de satélite de alta resolução',
       benefits: [
         'Imagens de alta resolução fornecidas pelo Google.',
-        'Contexto real para registros mais precisos.'
+        'Contexto real para registros mais precisos.',
       ],
       imageSrc: '/imgs/satellite-view-feature.png',
-      imageAlt: 'Imagem da plataforma em modo satélite, exibindo lotes sobrepostos em uma foto aérea de alta resolução'
+      imageAlt:
+        'Imagem da plataforma em modo satélite, exibindo lotes sobrepostos em uma foto aérea de alta resolução',
     },
-    
-
+    {
+      title: 'Planejamento com Modo Rascunho',
+      description:
+        'Planeje os lotes com o modo de rascunho, faça o esboço antes de finalizar o registro do lote.',
+      benefits: [
+        'Esboços livres: Desenhe e ajuste de forma livre.',
+        'Salve para depois: Guarde seus rascunhos para futura referência ou edição',
+        'Planejamento seguro: Finalize e salve o lote apenas quando tiver certeza.',
+      ],
+      imageSrc: '/imgs/draft-mode-feature.png',
+      imageAlt:
+        'Imagem do modo rascunho, mostrando um loteamento sendo esboçado no mapa antes de ser salvo.',
+    },
+    {
+      title: 'Registro de Imóvel Detalhado',
+      description:
+        'Complete o registro do seu imóvel com um formulário intuitivo e abrangente. Insira todas as informações essenciais, desde a matrícula no cartório até os detalhes do endereço e do loteamento.',
+      benefits: [
+        'Dados da Matrícula: Informe a matrícula do cartório, municipal, livro e ficha.',
+        'Endereço Completo: Preencha todos os campos de endereço para uma localização exata.',
+        'Detalhes do Lote: Especifique quadra, lote, loteamento e tamanho do imóvel.',
+      ],
+      imageSrc: '/imgs/form-feature.png',
+      imageAlt:
+        'Imagem do formulário de registro de imóvel com campos para matrícula, endereço e detalhes do lote',
+    },
   ];
 }
